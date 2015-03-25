1304b88938af256c21c64bab11110a7fd9d68cb8
Tyler Holbrook <tyjholbrook@gmail.com>
2015-03-25 16:50:04 -0600

    all in one changes

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by Andrew on 3/8/2015.
  */
-
+// Tyler's Stuff
 	var currentUser = "Tyler Holbrook";
 	function insertComment(string_in, checked)
 	{
@@ -19,3 +19,93 @@
 			privateCheckbox.checked = false;
 		}
 	}
+
+
+
+//Dantley's Stuff
+var imagelist = ["../Resources/cactus.jpeg", "../Resources/dog.jpeg", "../Resources/husky.jpeg", "../Resources/nike.jpeg", "../Resources/patriot.jpeg","../Resources/rooney.jpeg","../Resources/starwars.jpeg", "../Resources/sword.jpeg","../Resources/cow.jpeg","../Resources/greenChurch.jpeg","../Resources/dragon.jpeg","../Resources/moutain.jpeg","../Resources/roller.jpeg",];
+	var numberScrollRight = 6;
+	var numnberScrollLeft = imagelist.length-1;
+	var currentImageForLoad=0;
+	function initPicture()
+	{
+		document.getElementById("first").src = imagelist[0];
+		document.getElementById("second").src = imagelist[1];
+		document.getElementById("third").src = imagelist[2];
+		document.getElementById("fourth").src = imagelist[3];
+		document.getElementById("fifth").src = imagelist[4];
+		document.getElementById("sixth").src = imagelist[5];
+
+	}
+
+function getImages() 
+{
+
+	var imgs = document.getElementsByTagName("img");
+	//alert(imgs.length);
+	var imgSrcs = [];
+
+    for (var i = 0; i < imgs.length; i++) {
+        imgSrcs.push(imgs[i].src);
+    }
+    
+	return imgSrcs;
+   // alert("test");
+}
+
+function rightArrow () 
+{
+	document.getElementById("first").src=document.getElementById("second").src;
+	document.getElementById("second").src=document.getElementById("third").src;
+	document.getElementById("third").src=document.getElementById("fourth").src;
+	document.getElementById("fourth").src=document.getElementById("fifth").src;
+	document.getElementById("fifth").src=document.getElementById("sixth").src;
+	//alert(numberScrollRight);
+	document.getElementById("sixth").src=imagelist[numberScrollRight];
+	if(numberScrollRight==imagelist.length-1)
+	{
+		numberScrollRight=0;
+	}
+	else
+	{
+		numberScrollRight++;
+	}
+	if(numnberScrollLeft==imagelist.length-1)
+	{
+		numnberScrollLeft=0;
+	}
+	else
+	{
+		numnberScrollLeft++;
+	}
+
+}
+
+function leftArrow()
+{
+	document.getElementById("sixth").src=document.getElementById("fifth").src;
+	document.getElementById("fifth").src=document.getElementById("fourth").src;
+	document.getElementById("fourth").src=document.getElementById("third").src;
+	document.getElementById("third").src=document.getElementById("second").src;
+	document.getElementById("second").src=document.getElementById("first").src;
+	//alert(numnberScrollLeft);
+	document.getElementById("first").src=imagelist[numnberScrollLeft];
+	if (numnberScrollLeft ==0)
+	{
+		numnberScrollLeft = imagelist.length-1;
+	}
+	else
+	{
+		numnberScrollLeft--;
+	}
+	if (numberScrollRight ==0)
+	{
+		numberScrollRight = imagelist.length-1;
+	}
+	else
+	{
+		numberScrollRight--;
+	}
+
+
+}
