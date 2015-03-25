cc9dabad9b31a2d428d6b83eecf82b4fb26f18b7
Tyler Holbrook <tyjholbrook@gmail.com>
2015-03-25 16:13:16 -0600

    began combining

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,3 +1,21 @@
 /**
  * Created by Andrew on 3/8/2015.
  */
+
+	var currentUser = "Tyler Holbrook";
+	function insertComment(string_in, checked)
+	{
+		if (string_in != "")
+		{
+			var output = document.getElementById("commentshere").innerHTML;
+			output += "<p class='aComment'>";
+			output += currentUser;
+			output +=": ";
+			if (checked) output += "(Private) ";
+			output += string_in;
+			output += "</p>";
+			document.getElementById("commentshere").innerHTML = output;
+			thecomment.value = "";
+			privateCheckbox.checked = false;
+		}
+	}
